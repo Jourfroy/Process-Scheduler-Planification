87a3f89c1a20cd06856e1492a116397d25968343
Jourfroy <ijtsakem@gmail.com>
2024-11-09 10:34:25 +0100

    Update FCFS.cpp

--- projet test/FCFS.cpp
+++ projet test/FCFS.cpp
@@ -31,7 +31,7 @@ void algorithme_FCFS_NonPreemptif (std::vector <Process>& processus) {
         int secondeCPU = 0;
 
         if (processus[i-1].getm_presenceMemoire() == "Charge En Memoire"){
-            // Minuteur pour simuler l'exécution CPU
+            // Minuteur pour simuler l'exÃ©cution CPU
             cout << "------------------------------------------------"<< endl;
             cout << "Le processus caracterise par :" << endl;
             cout << "-->Temps d'arrivee :  " << processus[i-1].getm_tempsArrive () << " seconde(s)" << endl;
@@ -44,7 +44,7 @@ void algorithme_FCFS_NonPreemptif (std::vector <Process>& processus) {
             processus[i-1].setm_tempsExecution (processus[i-1].getm_tempsAttente() + processus[i-1].getm_tempsCpu() + processus[i-1].getm_tempsES());
 
             while (secondeCPU <= processus[i-1].getm_tempsCpu()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Attendre une seconde par unité de temps CPU
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Attendre une seconde par unitÃ© de temps CPU
                 secondeCPU++;
                 secondeMemoire++;
                 secondeES++;
@@ -78,7 +78,7 @@ void algorithme_FCFS_NonPreemptif (std::vector <Process>& processus) {
             cout << "Aucun processus charge en memoire !!!" << endl;
             cout << "Veuillez patienter pendant " << processus[i-1].getm_tempsArrive() - secondeMemoire << " seconde(s) le temps que le processus " << processus[i-1].getm_name () << " soit lancer et charge en memoire pour etre executer !!!" << endl;
             while (secondeMemoire <= processus[i-1].getm_tempsArrive()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Attendre une seconde par unité de temps
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Attendre une seconde par unitÃ© de temps
                 secondeMemoire++;
             }
 
@@ -99,81 +99,3 @@ void algorithme_FCFS_NonPreemptif (std::vector <Process>& processus) {
         processus[i].setm_etat("Execution complete");
     }
 }
-
-
-/*
-void algorithme_FCFS_Preemptif (std::vector <Process>& processus){
-    std::sort(processus.begin(), processus.end(), compareInstantArrive);
-
-    processus[0].setm_presenceMemoire ("Charge En Memoire");
-
-    double memoireDispo = 100.0 ;
-    int secondeMemoire = 0;
-    int secondeES = 0;
-
-
-    for (int i =1; i <= processus.size(); i++) {
-        int secondeCPU = 0;
-
-        if (processus[i-1].getm_presenceMemoire() == "Charge En Memoire"){
-            // Minuteur pour simuler l'exécution CPU
-            cout << "------------------------------------------------"<< endl;
-            cout << "Le processus caracterise par :" << endl;
-            cout << "-->Niveau Priorite :" << processus[i-i].getm_priorite () << endl;
-            cout << "-->Temps d'arrivee :" << processus[i-1].getm_tempsArrive ()<< endl;
-            cout << "-->PID :            " << processus[i-1].getm_PID () << endl;
-            cout << "-->Nom :            " << processus[i-1].getm_name () <<endl;
-            while (secondeCPU <= processus[i-1].getm_tempsCpu()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(20));  // Attendre une seconde par unité de temps CPU
-                secondeCPU++;
-                secondeMemoire++;
-                secondeES++;
-
-                //Verification si un processus ne devait pas etre charge en memoire pendant l,execution de celui-ci
-                for (int j =i; j< processus.size(); j++) {
-                    if (secondeMemoire == processus[j].getm_tempsArrive()) {
-                        if (memoireDispo >= processus[j].getm_memoire()){
-                            processus[j].setm_presenceMemoire("Charge En Memoire");
-                        }else {
-                            processus[j].setm_presenceMemoire("Liste d'attente");
-                        }
-                    }
-                }
-
-                //Verification si le temps entree/sortie d'un processus d'un processus deja executer n'est pas arrive
-                //Si la condition ci-dessus est verifier, je sais que les i-1 processus ont ete executer avec succes
-                for (int j = 0; j < i-1; j++ ) {
-                    processus[j].setm_tempsES_Ecoule(processus[j].getm_tempsES_Ecoule()+1);
-                    if (processus[j].getm_tempsES_Ecoule() >= processus[j].getm_tempsES() ){
-                        processus[j].setm_presenceMemoire("");
-                        processus[j].setm_etat ("Execution complete");
-                        memoireDispo += processus[j].getm_memoire();
-                    }
-                }
-
-            }
-            cout << "Processus executer avec succes mais il sera encore present en memoire pour " << processus[i-1].getm_tempsES() << " seconde(s) !!!" <<endl;
-        }else {
-
-            cout << "Aucun processus charge en memoire !!!" << endl;
-            cout << "Veuillez patienter pendant " << processus[i-1].getm_tempsArrive() - secondeMemoire << " seconde(s) le temps que le processus " << processus[i-1].getm_name () << " soit lancer et charge en memoire pour etre executer !!!" << endl;
-            while (secondeMemoire <= processus[i-1].getm_tempsArrive()) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(20));  // Attendre une seconde par unité de temps
-                secondeMemoire++;
-            }
-
-            //Verification si le temps entree/sortie d'un processus d'un processus deja executer n'est pas arrive
-            for (int j = 0; j < i-1; j++ ) {
-                processus[j].setm_tempsES_Ecoule(processus[j].getm_tempsES_Ecoule()+1);
-                if (processus[j].getm_tempsES_Ecoule() >= processus[j].getm_tempsES() ){
-                    processus[j].setm_presenceMemoire("");
-                    processus[j].setm_etat ("Execution complete");
-                    memoireDispo += processus[j].getm_memoire();
-                }
-            }
-        }
-
-    }
-
-}
-*/
